d279e6e7816f639a55198bdb9eacaa081a309fa4
matryxxx02 <nicolas.fernandes.etu@univ-lille.fr>
2021-04-25 09:01:03 +0200

    foc

--- src/main/java/FileWatcher/CustomFileAlterationListener.java
+++ src/main/java/FileWatcher/CustomFileAlterationListener.java
@@ -6,43 +6,76 @@ import org.apache.commons.io.monitor.FileAlterationObserver;
 import java.io.File;
 
 public class CustomFileAlterationListener implements FileAlterationListener {
+
+    /**
+     *
+     * @param fileAlterationObserver
+     */
     @Override
     public void onStart(FileAlterationObserver fileAlterationObserver) {
     }
 
+    /**
+     *
+     * @param file
+     */
     @Override
     public void onDirectoryCreate(File file) {
         System.out.println("Directory created : " + file.getAbsolutePath());
         //TODO : /post server
     }
 
+    /**
+     *
+     * @param file
+     */
     @Override
     public void onDirectoryChange(File file) {
         System.out.println("Directory changed : " + file.getAbsolutePath());
     }
 
+    /**
+     *
+     * @param file
+     */
     @Override
     public void onDirectoryDelete(File file) {
         System.out.println("Directory deleted : " + file.getAbsolutePath());
         //TODO : delete /path
     }
 
+    /**
+     *
+     * @param file
+     */
     @Override
     public void onFileCreate(File file) {
         System.out.println("File created : " + file.getAbsolutePath())  ;
     }
 
+    /**
+     *
+     * @param file
+     */
     @Override
     public void onFileChange(File file) {
         System.out.println("File changed : " + file.getAbsolutePath());
         //TODO :
     }
 
+    /**
+     *
+     * @param file
+     */
     @Override
     public void onFileDelete(File file) {
         System.out.println("File deleted : " + file.getAbsolutePath());
     }
 
+    /**
+     *
+     * @param fileAlterationObserver
+     */
     @Override
     public void onStop(FileAlterationObserver fileAlterationObserver) {
     }
